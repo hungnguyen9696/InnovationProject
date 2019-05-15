3eb7a24249e9c2f35b03e9c02a48a94f80b29ce4
hungnguyen9696 <hungnvh96@gmail.com>
2019-05-15 19:05:06 +0300

    delete no need file

--- client/src/App.js
+++ client/src/App.js
@@ -33,8 +33,6 @@ import PostsTravel from "./components/posts/PostsTravel";
 import PostsBusiness from "./components/posts/PostsBusiness";
 import PostsMovies from "./components/posts/PostsMovies";
 
-import Youtube from "./components/youtube/Youtube";
-
 import "./App.css";
 
 // Check for token
@@ -71,7 +69,6 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
               <Route exact path="/profile/:handle" component={Profile} />
-              <Route exact path="/youtube" component={Youtube} />
 
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
